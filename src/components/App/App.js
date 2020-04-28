bfa60549b303b3fa7c657e2eed7971beddc2fc7b
Freddie Levenson <freddielev@gmail.com>
2020-04-28 17:19:48 -0600

    Remove Boilerplate react code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { fetchRestaurants } from '../../utilities';
 import RestaurantContext from '../../context/restaurantContext'
@@ -24,20 +23,8 @@ function App() {
     }>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
 
-          <p></p>
+          <p>Hello</p>
         </header>
       </div>
     </RestaurantContext.Provider>
