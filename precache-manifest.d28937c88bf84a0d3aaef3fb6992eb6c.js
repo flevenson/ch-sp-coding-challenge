7935f0c04806309013072d65e537e29cdad0e4ae
Freddie Levenson <freddielev@gmail.com>
2020-04-29 21:14:06 -0600

    Updates

---
+++ precache-manifest.d28937c88bf84a0d3aaef3fb6992eb6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6ae79256c2efa83633bf84be0ae86c",
+    "url": "/ch-sp-coding-challenge/index.html"
+  },
+  {
+    "revision": "93593bdbea07e9de01d7",
+    "url": "/ch-sp-coding-challenge/static/css/main.32925c20.chunk.css"
+  },
+  {
+    "revision": "f9a9c2680ec3c31d3205",
+    "url": "/ch-sp-coding-challenge/static/js/2.bb259421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ch-sp-coding-challenge/static/js/2.bb259421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93593bdbea07e9de01d7",
+    "url": "/ch-sp-coding-challenge/static/js/main.4463c833.chunk.js"
+  },
+  {
+    "revision": "bb9651fca0491657e14e",
+    "url": "/ch-sp-coding-challenge/static/js/runtime-main.f0916315.js"
+  }
+]);
