1ebcee58d72e0674e4e327b36731de6105b9f81f
Freddie Levenson <freddielev@gmail.com>
2020-04-29 22:10:39 -0600

    Remove Comment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,8 +22,6 @@ function App() {
 
   const makeGenresList = (restaurants) => {
     let genresNoDupes = []
-    // let genresArray = restaurants.map(restaurant => restaurant.genre)
-    // let genresAsArray = genresArray.map(genreString => genreString.split(","))
     restaurants.forEach(restaurant => {
       for(let i = 0; i < restaurant.genre.length; i++) {
         if(!genresNoDupes.includes(restaurant.genre[i])){
